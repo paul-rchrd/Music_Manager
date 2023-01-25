911db7d33b8151e0fb93636a29205a3f0d3c6a33
Jonas Hampel <98750204+Deviljho35@users.noreply.github.com>
2023-01-25 09:59:49 +0100

    Verdeutlichung des Aufbaus
    Schauen sie sich auch meine anderen Projekte an

--- Playlist.java
+++ Playlist.java
@@ -1,14 +1,23 @@
+//Importiert alle wichtigen Methoden / Utilities
 import java.util.*;
+
+// Erstellt die Klasse Playlist
 public class Playlist {
- private String bezeichnung;
- private ArrayList<Song> songs;
- public Playlist(String bez){
+   
+    // Attribute 
+private String bezeichnung;
+private ArrayList<Song> songs;
+
+// Konstruktor (Macht konstruktionen, wie Objekte(eigentlich nur Objekte))
+public Playlist(String bez){
     this.bezeichnung = bez;
+ }  
 
- }   
+ // Methode zum Hinzufügen von Songs mithilfe von dem Attribut song
  public void hinzufuegenSong(Song song){
-
  }
+
+ // Methode um die Bezeichnung zurückzuholen
  public String getBezeichnung(){
     return bezeichnung;
  }
