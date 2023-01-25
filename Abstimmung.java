59db8d86d198353103d0945111438c21f396dcc4
Paul Aßmann <assmann.paul@gmx.de>
2023-01-25 09:35:30 +0100

    Klassenstrukturen fertig
    .

---
+++ Abstimmung.java
@@ -0,0 +1,56 @@
+import java.util.*;
+public class Abstimmung {
+
+    private String beziehung;
+    private int abstimmdauer;
+    private int spieldauer;
+    private Date startzeit;
+    private ArrayList<Song> songs = new ArrayList<Song>();
+
+    public Abstimmung(String bez, int ad, int sd){
+        this.spieldauer = sd;
+        this.abstimmdauer = ad;
+        this.beziehung = bez;
+
+    }
+
+    public void hinzufuegen(Song song){
+
+    }
+
+    public boolean starten(){
+        return true;
+    }
+
+
+
+    public Playlist generierePlaylist(){
+        Playlist p1;
+        p1 = new Playlist();
+        return ;
+    }
+
+    public boolean istAktiv(){
+        return ture;
+    }
+
+    public String getBeziehung() {
+        return beziehung;
+    }
+
+    public int getAbstimmdauer() {
+        return abstimmdauer;
+    }
+
+    public int getSpieldauer() {
+        return spieldauer;
+    }
+
+    public Date getStartzeit() {
+        return startzeit;
+    }
+
+    public void setStartzeit(Date startzeit) {
+        this.startzeit = startzeit;
+    }
+}
---
+++ MusicManager.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class MusicManager {
+
+    private ArrayList<Playlist> playlists;
+    private Abstimmung abstimmung;
+
+    public MusicManager(){
+        playlists = new ArrayList<Playlist>();
+    }
+
+    public boolean erzeugeAbstimmung(String bez, int ad, int sd ){
+
+
+        return true;
+    }
+
+    public boolean hinzufuegenSongsZurAbstimmung(){
+
+
+        return true;
+    }
+
+    public boolean starteAbstimmung(){
+        return true;
+    }
+
+    public String holeSongsDerAbstimmung(){
+        return "";
+    }
+
+    public Song sucheSong(int nr){
+        return ;
+    }
+
+    public boolean abstimmen(Song song){
+        return true;
+    }
+
+    public String holePlaylist(String beziehung){
+        return "";
+    }
+}
