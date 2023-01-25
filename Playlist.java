1b1acf88383fb6928cff466aafe6c030933cd428
Jonas Hampel <98750204+Deviljho35@users.noreply.github.com>
2023-01-25 09:36:17 +0100

    .
    .

---
+++ Playlist.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Playlist {
+ private String bezeichnung;
+ private ArrayList<Song> songs;
+ public Playlist(String bez){
+    this.bezeichnung = bez;
+
+ }   
+ public void hinzufuegenSong(Song song){
+
+ }
+ public String getBezeichnung(){
+    return bezeichnung;
+ }
+
+}
