6e3b0c9921f6a4d2e57f4bdf8ca91bf7f784c3cc
Jonas Hampel <98750204+Deviljho35@users.noreply.github.com>
2023-02-06 11:15:14 +0100

    Update MusicManager.java

--- MusicManager.java
+++ MusicManager.java
@@ -1,28 +1,41 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-
+import java.util.Scanner;
 public class MusicManager {
 
     private ArrayList<Playlist> playlists;
     private Abstimmung abstimmung;
     private ArrayList<Song> alleSongs;
-
+    private Connection conn;
+    private dataBase db;
+private Scanner sc = new Scanner(System.in);
     public MusicManager(){
         playlists = new ArrayList<Playlist>();
     }
 
-    public boolean erzeugeAbstimmung(String bez, int ad, int sd ){
-
-
+    public boolean einstellenSong(){
+    public boolean erzeugeAbstimmung(String bez, int ad, int sd ) throws SQLException{
+        Statement stm = conn.createStatement();
+        String einfügen = "INSERT INTO Song(autowert, songnr, titel, spieldauer, interpret, anzahlStimmen) VALUES( '"+bez+"','"+ad+"','"+sd+"');";
+    stm.executeUpdate(einfügen);
+         return true;   
+    }
+    public boolean erzeugeAbstimmung(String bez, int ad, int sd ) throws SQLException{
+    Statement stm = conn.createStatement();
+    String einfügen = "INSERT INTO Abstimmung(bezeichnung, abstimmdauer, spieldauer) VALUES( '"+bez+"','"+ad+"','"+sd+"');";
+    stm.executeUpdate(einfügen);
         return true;
     }
 
     public boolean hinzufuegenSongsZurAbstimmung(){
 
-
         return true;
     }
 
     public boolean starteAbstimmung(){
+        
         return true;
     }
 
